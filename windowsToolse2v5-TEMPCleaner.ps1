bcd2b267904772ad46f273965f2b735e364fe949
2vlad5 <vladislav123263@gmail.com>
2025-11-24 17:07:09 +0300

    v001

--- windowsToolse2v5-TEMPCleaner.ps1
+++
@@ -1,7 +0,0 @@
-function test {
-
- Add-Type -AssemblyName "System.Windows.Forms"
- [void][System.Windows.Forms.Messagebox]::Show("Установка скрипта для очистки временных файлов завершена!")
-}
-
-test
